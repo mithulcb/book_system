5af6c16e505c491437c2ce724c5fe1d72b0b175a
mithulcb <mithulcb@gmail.com>
2023-04-18 09:42:17 +0530

    proxy

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -10,7 +10,7 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        const res = await axios.get("http://localhost:8800/books");
+        const res = await axios.get("/books");
         setBooks(res.data);
       } catch (err) {
         console.log(err);
@@ -23,7 +23,7 @@ const Books = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:8800/books/${id}`);
+      await axios.delete(`/books/${id}`);
       window.location.reload()
     } catch (err) {
       console.log(err);
@@ -43,7 +43,7 @@ const Books = () => {
             <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
             <button className="update">
               <Link
-                to={`http://localhost:8800/update/${book.id}`}
+                to={`/update/${book.id}`}
                 style={{ color: "inherit", textDecoration: "none" }}
               >
                 Update
